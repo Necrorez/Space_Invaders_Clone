7fc0652aed21cadc32f552de7c436545252d36b5
necro <dalius.visokinskas@gmail.com>
2021-09-20 20:08:00 +0300

    added simple server

--- SpaceInvaders/src/SpaceX05/Client.java
+++ SpaceInvaders/src/SpaceX05/Client.java
@@ -1,2 +1,47 @@
-package SpaceX05;public class Client {
+package SpaceX05;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class Client
+{
+    private static BufferedReader reader;
+    private static PrintWriter writer;
+    private static String username;
+    private static Socket sock;
+    public static void main(String[] args) throws Exception
+    {
+        networkSetup();
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+    }
+    private static void networkSetup(){
+        try {
+            sock = new Socket("localhost", 4444);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            var scanner = new Scanner(System.in);
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("Networking Established");
+            while (scanner.hasNextLine()) {
+                writer.println(scanner.nextLine());
+            }
+
+        } catch (IOException ex) {System.out.println("No network access");}
+    }
+    public static class IncomingReader implements Runnable {
+        public void run() {
+            try {
+                while (reader.readLine() != null) {
+                    System.out.println(reader.readLine());
+                }
+            } catch (Exception ex) {ex.printStackTrace();}
+        }
+    }
+
 }
--- SpaceInvaders/src/SpaceX05/Server.java
+++ SpaceInvaders/src/SpaceX05/Server.java
@@ -1,8 +1,85 @@
 package SpaceX05;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
 
-public class Server {
+public class Server
+{
+    ArrayList<PrintWriter> clientOutStream;
 
-    public static void main(String[] args) {
-	// write your code here
+    public class Game implements Runnable{
+        BufferedReader reader;
+        Socket socket;
+        public Game(Socket clientSocket){
+            try{
+                socket = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            String data;
+            try {
+                clientOutStream.forEach(client-> System.out.println(client));
+                while ((data = reader.readLine()) != null) {
+                    sendData(data);
+
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception
+    {
+        new Server().start();
+    }
+
+    public void start(){
+        clientOutStream = new ArrayList<PrintWriter>();
+        try {
+            ServerSocket serverSocket = new ServerSocket(4444);
+            while (true){
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutStream.add(writer);
+                Thread t = new Thread(new Game(clientSocket));
+                t.start();
+                System.out.println("Player Connected");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendData(String data){
+        Iterator<PrintWriter> writerIterator = clientOutStream.iterator();
+        while (writerIterator.hasNext()){
+            try {
+                System.out.println("sending");
+                PrintWriter writer = writerIterator.next();
+                writer.println(data);
+                writer.flush();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
+
+
+
+
+
+
