95847461d271c05a5d9d04ee46fca7816c7de7cd
Tomras2 <tomorune@gmail.com>
2021-10-12 00:36:46 +0300

    Abstract factory with 3 factories

--- SpaceInvaders/src/SpaceX05/Factory/BalancedAliensFactory.java
+++ SpaceInvaders/src/SpaceX05/Factory/BalancedAliensFactory.java
@@ -6,31 +6,14 @@ import SpaceX05.Aliens.BalancedSquid;
 import SpaceX05.Aliens.BalancedUfo;
 public class BalancedAliensFactory extends AbstractFactory {
 
-    //private int alienId = 0;
     private String squidImg ="/SpaceX05/Images/Squid.jpg";
     private String crabImg ="/SpaceX05/Images/Crab.jpeg";
     private String ufoImg ="/SpaceX05/Images/UFO.jpeg";
 
-//    @Override
-//     public Alien factoryMethod(String param, int PosX, int PosY){
-//        switch (param){
-//            case "Squid":
-//                alienId++;
-//                return new BalancedSquid(squidImg,alienId,PosX,PosY);
-//            case "Crab":
-//                alienId++;
-//                return new BalancedCrab(crabImg,alienId,PosX,PosY);
-//            case "Ufo":
-//                alienId++;
-//                return new BalancedUfo(ufoImg,alienId,PosX,PosY);
-//            default:
-//                return null;
-//        }
-//    }
-@Override
+    @Override
     public Alien spawnCrab(String param, int alienId, int PosX, int PosY)
 {
-    return new BalancedCrab(crabImg,alienId,PosX,PosY);
+       return new BalancedCrab(crabImg,alienId,PosX,PosY);
 }
     @Override
     public Alien spawnSquid(String param, int alienId, int PosX, int PosY)
@@ -38,10 +21,13 @@ public class BalancedAliensFactory extends AbstractFactory {
 
         return new BalancedSquid(squidImg,alienId,PosX,PosY);
     }
+
     @Override
-    public Alien spawnUfo(String param, int alienId, int PosX, int PosY) {
+    public Alien spawnUfo(String param, int alienId, int PosX, int PosY)
+    {
         return new BalancedUfo(ufoImg,alienId,PosX,PosY);
     }
+
     @Override
     public Alien getAlienShip(String param){
         return new Alien() {
