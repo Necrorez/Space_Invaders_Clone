9ec52d219eb77dc6a2f1d29ccc36a91aabbc97ab
laurys15951 <laurys.strav@gmail.com>
2021-10-12 10:53:52 +0300

    Create Settings.java

---
+++ SpaceInvaders/src/SpaceX05/Settings.java
@@ -0,0 +1,25 @@
+package SpaceX05;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Settings {
+
+    private static Settings single_instance = null;
+
+    public int Board_Width;
+    public int Board_Height;
+
+    private Settings()
+    {
+        Board_Width =358;//358
+        Board_Height=500;//350
+    }
+
+    public static Settings getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Settings();
+
+        return single_instance;
+    }
