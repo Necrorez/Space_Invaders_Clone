a9f9e6cc1ef1128bb42566602244128374183e71
Tomras2 <tomorune@gmail.com>
2021-10-17 15:17:18 +0300

    First test try

--- SpaceInvaders/src/SpaceX05/Factory/AbstractFactory.java
+++ SpaceInvaders/src/SpaceX05/Factory/AbstractFactory.java
@@ -3,7 +3,7 @@ package SpaceX05.Factory;
 import SpaceX05.Alien;
 
 public  abstract  class AbstractFactory {
-    
+
     public abstract Alien spawnCrab(String param, int alienId, int PosX, int PosY);
     public abstract Alien spawnSquid(String param, int alienId, int PosX, int PosY);
     public abstract Alien spawnUfo(String param, int alienId, int PosX, int PosY);
